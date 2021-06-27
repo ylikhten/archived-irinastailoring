4a891cd06ed8e3e250ae38a96ee7cb71c82fee99
ylikhten <ylikhten@mymail.mines.edu>
2021-06-26 22:37:35 -0600

    date added to reviews; structured reviews on review page

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -11,11 +11,24 @@ module.exports.reviewList = function (req, res) {
   var revs = [];
   Rev.find({}, function(err, allReviews) {
     if (err) {
-      console.log(err);
+      //console.log(err);
       sendJsonResponse(res, 404, err);
     } else {
-      revs = allReviews;
-      console.log(revs);
+      //revs = allReviews;
+      allReviews.forEach(
+        function(rev) {
+          if(rev.date != null){
+            var temp = {};
+            temp.name = rev.name;
+            temp.date = rev.date.toDateString();
+            temp.review = rev.review;
+            revs.push(temp);
+            
+            console.log(temp.date);
+          }
+        }
+      );
+      //console.log(revs[3].date);
       sendJsonResponse(res, 200, revs);
     }
   });
