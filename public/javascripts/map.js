2179bb4c7da096027f6af4a60759069068be9724
ylikhten <ylikhten@mymail.mines.edu>
2021-06-27 17:02:25 -0600

    updated buttons and review page

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,7 +3,7 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
-new Map({
+/*new Map({
   target: 'map',
   layers: [
     new TileLayer({
@@ -15,5 +15,24 @@ new Map({
   view: new View({
     center: [0, 0],
     zoom: 2
+  })
+});*/
+
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      title: 'Global Imagery',
+      source: new ol.source.TileWMS({
+        url: 'http://demo.opengeo.org/geoserver/wms',
+        params: {LAYERS: 'nasa:bluemarble', VERSION: '1.1.1'}
+      })
+    })
+  ],
+  view: new ol.View({
+    projection: 'EPSG:4326',
+    center: [0, 0],
+    zoom: 0,
+    maxResolution: 0.703125
   })
 });
